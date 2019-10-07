37e7ae711a880933ead185084b7b59ee28869f6f
Felix van Deelen <felix.van.deelen@nos.nl>
2019-10-07 16:16:59 +0200

    Implemented basic analysis for the complexity of words

--- app/flask_app.py
+++ app/flask_app.py
@@ -34,13 +34,18 @@ def get_article_text(id):
     for paragraph in paragraphs:
         article_text += ' ' + paragraph.get_text()
 
+    print(article_text)
     return article_text
 
 '''
 Get a predefined list of difficult words
 '''
 def get_difficult_words():
-    file = open('data/words.csv', 'r')
+    local = True
+    if (local):
+        file = open('app/data/words.csv', 'r')
+    else:
+        file = open('data/words.csv', 'r')
     difficult_words = file.read().split(',')
     file.close()
     return difficult_words
@@ -51,12 +56,80 @@ TODO: Fetch the definition(s) of the difficult word
 def get_definitions(word):
     return ['none yet']
 
+'''
+remove all characters other than letters from string
+'''
+def cleanup_word(input):
+    valids = []
+    for character in input:
+        if character.isalpha():
+            valids.append(character)
+    return ''.join(valids)
+'''
+Count the number of syllables in word
+'''
+def syllable_count(word): ## todo: this is up for improvement: written for english not dutch, what about oe, au, ui etc
+    word = word.lower()
+    count = 0
+    vowels = "aeiouy"
+    if word[0] in vowels:
+        count += 1
+    for index in range(1, len(word)):
+        if word[index] in vowels and word[index - 1] not in vowels:
+            count += 1
+    if word.endswith("e"):
+        count -= 1
+    if count == 0:
+        count += 1
+    return count
+
+
+
+'''
+Basic word complexity analysis. A word is complex when one of the following is true:
+    - Not capitalized (todo: exclude start of sentence)
+    - word length > 10 characters
+    - more than four syllables
+    - word contains x, y, c, ch, ae, ea, q, z and word length > 6 characters
+'''
+def get_difficult_words_alternative(article_text):
+    ### CONFIG
+    max_word_length = 10
+    max_syllables = 4
+    danger_letters = ['x', 'y', 'c', 'ch', 'ae', 'ea', 'q', 'z']
+    danger_letters_max_length = 6
+
+    ### Functionality
+    all_words = article_text.split()
+
+    to_return = []
+    for word in all_words: # todo: uniques
+
+        word = cleanup_word(word)
+
+        if (len(word) <= 0):
+            continue
+        elif (word[0].isupper()):
+            continue # todo: up for improvement, word should not be skipped when at the start of a sentence
+        elif (len(word) >= max_word_length):
+            to_return.append(word)
+        elif (syllable_count(word) >= max_syllables):
+            to_return.append(word)
+        elif (len(word) >= danger_letters_max_length):
+            for danger_letter in danger_letters:
+                if (danger_letter in word):
+                    to_return.append(word)
+                    break
+
+    return set(list(to_return))
+
 
 @app.route('/items/<int:id>')
 def get_difficults_words(id):
     article_text = get_article_text(id)
 
-    difficult_words = get_difficult_words()
+    difficult_words = get_difficult_words_alternative(article_text) # this is based upon textual analysis
+    # difficult_words = get_difficult_words() # this is based upon a pre-defined list
 
     words_to_explain = []
     for word in difficult_words:
